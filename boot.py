9deac850c764f7f8c401a9a2be66cb6ffab3ce6c
Takao Akaki <mongonta@gmail.com>
2021-02-07 17:03:46 +0900

    Update boot.py
    refactoring and add copyright

--- boot.py
+++ boot.py
@@ -1,24 +1,34 @@
+#####################################################################
+# Copyright (c) 2021 Takao Akaki. All rights reserved.
+#####################################################################
+
+
 import time
 from Maix import GPIO, I2S
 from fpioa_manager import *
 from board import board_info
 import os, Maix, lcd, image
 from pmu import axp192
+from machine import I2C
 
 pmu = axp192()
 pmu.enablePMICSleepMode(True)
 
 sample_rate   = 16000
-words = ["1", "2", "3"]
 
 #####################################################################
+# Initial values
+#####################################################################
+
+# If you increase the words array, the number of registered words
+# will also increase.
+words = ["1", "2", "3"]
+
 # If the word recognition rate is low, look at the values of
 # dtw_value and current_frame_len in the terminal and adjust them.
 dtw_threshold = 400
 frame_len_threshold = 60
-#####################################################################
 
-#####################################################################
 # Settings for M5StickV. Maix series should fix the following lines.
 lcd.init(type=3)
 fm.register(board_info.MIC_DAT,fm.fpioa.I2S0_IN_D0, force=True)
@@ -29,38 +39,33 @@ button_a = GPIO(GPIO.GPIO1, GPIO.IN, GPIO.PULL_UP)
 
 # data storage "/sd/" or "/flash/"
 storage = "/sd/"
-#####################################################################
 
-lcd.rotation(0)
-lcd_w = lcd.width()
-lcd_h = lcd.height()
-img = image.Image(size=(lcd_w, lcd_h))
-img.draw_rectangle((0, 0, lcd_w, lcd_h), fill=True, color=(255, 255, 255))
-img.draw_string(10, 10, "MaixPy", color=(255, 0, 0), scale=1, mono_space=0)
-img.draw_string(10, 50, "Isolated words Recognizer", color=(0, 0, 255), scale=1, mono_space=0)
-lcd.display(img)
-rx = I2S(I2S.DEVICE_0)
-rx.channel_config(rx.CHANNEL_0, rx.RECEIVER, align_mode=I2S.STANDARD_MODE)
-rx.set_sample_rate(sample_rate)
-print(rx)
-from speech_recognizer import isolated_word
-# model
-sr = isolated_word(dmac=2, i2s=I2S.DEVICE_0, size=50)
-print(sr.size())
-print(sr)
-## threshold
-sr.set_threshold(0, 0, 10000)
+
+#####################################################################
+# Functions
+#####################################################################
+def print_lcd(str1, str2=None, str3=None, serial_out=True, bgcolor=(0, 0, 0)):
+    img.draw_rectangle((0, 0, lcd_w, lcd_h), fill=True, color=bgcolor)
+    if (str1 != None):
+        img.draw_string(10, 10, str1, color=(255, 0, 0), scale=2, mono_space=0)
+        if (serial_out):
+            print(str1)
+    if (str2 != None):
+        img.draw_string(10, 45, str2, color=(0, 0, 255), scale=2, mono_space=0)
+        if (serial_out):
+            print(str2)
+    if (str3 != None):
+        img.draw_string(10, 70, str3, color=(0, 0, 255), scale=3, mono_space=0)
+        if (serial_out):
+            print(str3)
+    lcd.display(img)
+
 
 def save_file(number, data):
-    img.draw_rectangle((0, 0, lcd_w, lcd_h), fill=True, color=(255, 255, 255))
-    img.draw_string(10, 10, "Data Saving...", color=(255, 0, 0), scale=2, mono_space=0)
+    print_lcd("Data Saving...")
     lcd.display(img)
     filename0 = storage + "rec0_" + str(number) + ".sr"
     filename1 = storage + "rec1_" + str(number) + ".sr"
-    print(filename0)
-    print(filename1)
-    print(len(data))
-    print(type(data))
     t0 = data[0]
     t1 = data[1]
     print(type(t0))
@@ -71,9 +76,7 @@ def save_file(number, data):
         f.write(bytearray(t1))
 
 def load_data(number):
-    img.draw_rectangle((0, 0, lcd_w, lcd_h), fill=True, color=(255, 255, 255))
-    img.draw_string(10, 10, "Data loading...", color=(255, 0, 0), scale=2, mono_space=0)
-    lcd.display(img)
+    print_lcd("Data Loading...")
     for i in range(number):
         print("load_data:" + str(i))
         filename0 = storage + "rec0_" + str(i) + ".sr"
@@ -87,49 +90,62 @@ def load_data(number):
         tupledata = [int(data0), data1]
         sr.set(i*2, tupledata)
         print(b'0x0d0x0a')
-## record and get & set
+
 def record_voice():
     for i in range(len(words)):
         while True:
             time.sleep_ms(100)
             print(sr.state())
             if sr.Done == sr.record(i*2):
+                print_lcd("get !")
                 data = sr.get(i*2)
-                print(len(data))
-                print(type(data))
                 save_file(i, data)
                 print(data)
                 break
             if sr.Speak == sr.state():
-                print('speak ' + words[i])
-                img.draw_rectangle((0, 0, lcd_w, lcd_h), fill=True, color=(255, 255, 255))
-                img.draw_string(10, 10, "Please speak " + words[i], color=(255, 0, 0), scale=2, mono_space=0)
-                lcd.display(img)
+                print_lcd("Please speak " + words[i])
         sr.set(i*2, data)
-        img.draw_rectangle((0, 0, lcd_w, lcd_h), fill=True, color=(255, 255, 255))
-        img.draw_string(10, 10, "get !", color=(255, 0, 0), scale=4, mono_space=0)
-        lcd.display(img)
         time.sleep_ms(500)
 
+##############################################################################
+# Main
+##############################################################################
+lcd.rotation(0)
+# set lcd backlight for M5StickV
+i2c = I2C(I2C.I2C0, freq=400000, scl=28, sda=29)
+i2c.writeto_mem(0x34, 0x91, b'\xa0')
 
-## recognizer
+lcd_w = lcd.width()
+lcd_h = lcd.height()
+img = image.Image(size=(lcd_w, lcd_h))
+print_lcd("MaixPy", "Isolated words Recognizer")
+rx = I2S(I2S.DEVICE_0)
+rx.channel_config(rx.CHANNEL_0, rx.RECEIVER, align_mode=I2S.STANDARD_MODE)
+rx.set_sample_rate(sample_rate)
+print(rx)
+from speech_recognizer import isolated_word
+# model
+sr = isolated_word(dmac=2, i2s=I2S.DEVICE_0, size=50)
+print(sr.size())
+print(sr)
+## threshold
+sr.set_threshold(0, 0, 10000)
+
+
+
+# data set
 try:
     load_data(len(words))
 except Exception as e:
     record_voice()
 
-print('Recognition begin')
-img.draw_rectangle((0, 0, lcd_w, lcd_h), fill=True, color=(255, 255, 255))
-img.draw_string(10, 10, "Recognition begin", color=(255, 0, 0), scale=2, mono_space=0)
-lcd.display(img)
+# recognition
+print_lcd("Recognition begin")
 time.sleep_ms(1000)
 
 while True:
     time.sleep_ms(200)
-    img.draw_rectangle((0, 0, lcd_w, lcd_h), fill=True, color=(255, 255, 255))
-    img.draw_string(10, 10, "Please speak word!", color=(255, 0, 0), scale=2, mono_space=0)
-    img.draw_string(10, 50, "BtnA: Record Voice", color=(0, 0, 255), scale=2, mono_space=0)
-    lcd.display(img)
+    print_lcd("Please speak word!", "BtnA: Record Voice", serial_out=False)
     if (button_a.value() == 0):
         record_voice()
     if sr.Done == sr.recognize():
@@ -139,8 +155,5 @@ while True:
             print(str(res[0]))
             for i in range(len(words)):
                 if res[0] == (i * 2):
-                    img.draw_rectangle((10, 10, lcd_w, lcd_h), fill=True, color=(255, 255, 255))
-                    img.draw_string(int(lcd_w / 2), 30, words[i], color=(255, 0, 0), scale=5, mono_space=0)
-                    lcd.display(img)
+                    print_lcd("Recognize",str3=words[i], bgcolor=(255, 255, 255))
                     time.sleep_ms(200)
-
