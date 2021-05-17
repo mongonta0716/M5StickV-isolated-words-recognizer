3b7818612522412eac268024ba5759ce14ad4973
Takao Akaki <mongonta@gmail.com>
2021-05-17 20:44:26 +0900

    Displays recording time and supports UART output

--- boot.py
+++ boot.py
@@ -15,7 +15,7 @@ from Maix import GPIO, I2S
 from fpioa_manager import *
 from board import board_info
 import os, Maix, lcd, image
-from machine import I2C
+from machine import I2C,UART
 
 if (BOARD_NAME == "M5STICKV"):
     from pmu import axp192
@@ -30,30 +30,37 @@ sample_rate   = 16000
 
 # If you increase the words array, the number of registered words
 # will also increase.
-words = ["1", "2", "3"]
+words = ["1", "2", "3", "4", "5"]
 
 # If the word recognition rate is low, look at the values of
 # dtw_value and current_frame_len in the terminal and adjust them.
 dtw_threshold = 400
 frame_len_threshold = 70
 
-# Settings for M5StickV. Maix series should fix the following lines.
 if (BOARD_NAME == "M5STICKV"):
+# GPIO Settings for M5StickV.
     lcd.init(type=3)
     fm.register(board_info.MIC_DAT,fm.fpioa.I2S0_IN_D0, force=True)
     fm.register(board_info.MIC_LRCLK,fm.fpioa.I2S0_WS, force=True)
     fm.register(board_info.MIC_CLK,fm.fpioa.I2S0_SCLK, force=True)
+    fm.register(34, fm.fpioa.UART2_TX, force=True)
+    fm.register(35, fm.fpioa.UART2_RX, force=True)
     fm.register(board_info.BUTTON_A, fm.fpioa.GPIO1, force=True)
-    button_a = GPIO(GPIO.GPIO1, GPIO.IN, GPIO.PULL_UP)
     button_a_label = "BtnA"
 elif ((BOARD_NAME == "MAIXDUINO") or (BOARD_NAME == "M1DOCK")):
+# GPIO Settings for Maixduino or M1Dock.
     lcd.init()
     fm.register(board_info.MIC0_DATA, fm.fpioa.I2S0_IN_D0, force=True)
     fm.register(board_info.MIC0_WS, fm.fpioa.I2S0_WS, force=True)
     fm.register(board_info.MIC0_BCK, fm.fpioa.I2S0_SCLK, force=True)
+    fm.register(21, fm.fpioa.UART2_TX, force=True)  # Maixduino PIN2
+    fm.register(22, fm.fpioa.UART2_RX, force=True)  # Maixduino PIN3
     fm.register(board_info.BOOT_KEY, fm.fpioa.GPIO1, force=True)
-    button_a = GPIO(GPIO.GPIO1, GPIO.IN, GPIO.PULL_UP)
     button_a_label = "BtnBoot"
+
+uart_port = UART(UART.UART2, 115200, 8, None, 1, timeout=1000, read_buf_len=4096)
+button_a = GPIO(GPIO.GPIO1, GPIO.IN, GPIO.PULL_UP)
+
 
 # data storage "/sd/" or "/flash/"
 storage = "/flash/"
@@ -62,18 +69,20 @@ storage = "/flash/"
 #####################################################################
 # Functions
 #####################################################################
-def print_lcd(str1, str2=None, str3=None, serial_out=True, bgcolor=(0, 0, 0)):
-    img.draw_rectangle((0, 0, lcd_w, lcd_h), fill=True, color=bgcolor)
+def print_lcd(str1=None, str2=None, str3=None, serial_out=True, bgcolor=(0, 0, 0)):
     if (str1 != None):
+        img.draw_rectangle((0, 0, lcd_w, 45), fill=True, color=bgcolor)
         img.draw_string(10, 10, str1, color=(255, 0, 0), scale=2, mono_space=0)
         if (serial_out):
             print(str1)
     if (str2 != None):
+        img.draw_rectangle((10, 45, lcd_w, 40), fill=True, color=bgcolor)
         img.draw_string(10, 45, str2, color=(0, 0, 255), scale=2, mono_space=0)
         if (serial_out):
             print(str2)
     if (str3 != None):
-        img.draw_string(10, 70, str3, color=(0, 0, 255), scale=3, mono_space=0)
+        img.draw_rectangle((10, 90, lcd_w, 40), fill=True, color=bgcolor)
+        img.draw_string(10, 90, str3, color=(0, 0, 255), scale=2, mono_space=0)
         if (serial_out):
             print(str3)
     lcd.display(img)
@@ -120,8 +129,14 @@ def record_voice():
                 save_file(i, data)
                 print(data)
                 break
+            for sec in range(3, 0, -1):
+                print_lcd("wait {} sec".format(sec))
+                time.sleep(1)
             if sr.Speak == sr.state():
-                print_lcd("Please speak " + words[i])
+                for sec in range(2, 0, -1):
+                    print_lcd("Please speak {}".format(words[i]), "remain {} sec".format(sec))
+                    time.sleep(1)
+
         sr.set(i*2, data)
         time.sleep_ms(500)
 
@@ -154,7 +169,6 @@ print(sr)
 sr.set_threshold(0, 0, 10000)
 
 
-
 # data set
 try:
     load_data(len(words))
@@ -173,10 +187,12 @@ while True:
     if sr.Done == sr.recognize():
         res = sr.result()
         print("(Number,dtw_value,currnt_frame_len,matched_frame_len)=" + str(res))
+        print_lcd(str3=str(res))
         if (res != None) and (res[1] < dtw_threshold) and (res[2] > frame_len_threshold):
             print(str(res[0]))
             for i in range(len(words)):
                 if res[0] == (i * 2):
-                    print_lcd("Recognize",str3=words[i], bgcolor=(255, 255, 255))
+                    print_lcd("Recognize",str3="Word: " + words[i], bgcolor=(0, 255, 255))
+                    # data_packet = bytearray([0xFF, 0x05, 0xFF, i]) # header FF05FF
+                    uart_port.write(words[i] + "\n")
                     time.sleep_ms(200)
-
